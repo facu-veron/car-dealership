deeca54eec86f87f0924d49fe193f6f3ae92bc91
Facundo Veron <faccu.veron92@gmail.com>
2023-10-06 13:22:55 -0300

    Se agregaron dtos para crear autos

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -5,12 +5,17 @@ import {
   Get,
   Param,
   ParseIntPipe,
+  ParseUUIDPipe,
   Patch,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
+@UsePipes(ValidationPipe)
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
   @Get()
@@ -18,15 +23,15 @@ export class CarsController {
     return this.carsService.findAll();
   }
   @Get(':id')
-  getCarByIi(@Param('id', ParseIntPipe) id: number) {
+  getCarByIi(@Param('id', ParseUUIDPipe) id: string) {
     console.log({ id });
 
     return { id: this.carsService.findOneById(id) };
   }
 
   @Post()
-  createCar(@Body() body: any) {
-    return body;
+  createCar(@Body() createCardDto: CreateCarDto) {
+    return createCardDto;
   }
 
   @Patch(':id')
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,17 +1,19 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { Car } from './interfaces/car.interface';
 
 @Injectable()
 export class CarsService {
-  private cars = [
-    { id: 1, brand: 'Toyota', model: 'Corolla' },
-    { id: 2, brand: 'Fiat', model: 'Argo' },
+  private cars: Car[] = [
+    { id: uuid(), brand: 'Toyota', model: 'Corolla' },
+    { id: uuid(), brand: 'Fiat', model: 'Argo' },
   ];
 
   findAll() {
     return this.cars;
   }
 
-  findOneById(id: number) {
+  findOneById(id: string) {
     const car = this.cars.find((car) => car.id === id);
     if (!car) throw new NotFoundException(`Car with id "${id}" not fond`);
 
